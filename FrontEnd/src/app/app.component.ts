3cc5142ce499b67fce1e3cc614d49286225e3581
progettoIngegneriaDelSw <progettoIngegneriaDelSw1@gmail.com>
2023-07-06 17:20:32 +0200

    modificato css
    Signed-off-by: progettoIngegneriaDelSw <progettoIngegneriaDelSw1@gmail.com>

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -5,8 +5,7 @@ import {User} from "./utilities/typeDB";
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
     public user!: User | null;
