5f4454d9ec234f4b362eaf970b45c4eae44a2600
progettoIngegneriaDelSw <progettoIngegneriaDelSw1@gmail.com>
2023-07-11 11:06:04 +0200

    sistemato details
    Signed-off-by: progettoIngegneriaDelSw <progettoIngegneriaDelSw1@gmail.com>

--- BackEnd/queries.js
+++ BackEnd/queries.js
@@ -320,7 +320,7 @@ const root = {
             JOIN inventory i ON i.film_id = f.film_id 
             JOIN rental r ON r.inventory_id = i.inventory_id
             LEFT JOIN payment p ON p.rental_id = r.rental_id
-            WHERE r.customer_id = 1
+            WHERE r.customer_id = $1
             ORDER BY duration`;
             const values = [args.customer_id];
             return db
