44f15497bbb0784967771e6cd99637510a485bb9
progettoIngegneriaDelSw <progettoIngegneriaDelSw1@gmail.com>
2023-07-12 15:27:08 +0200

    FINE PROGETTO
    Signed-off-by: progettoIngegneriaDelSw <progettoIngegneriaDelSw1@gmail.com>

--- BackEnd/queries.js
+++ BackEnd/queries.js
@@ -29,7 +29,7 @@ const root = {
                     },
                     SK,
                     {
-                        expiresIn: '10h'
+                        expiresIn: '2h'
                     });
                 return {
                     customer_id: user.customer_id,
